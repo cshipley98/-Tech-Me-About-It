9a34210342d057662a64b9887cfe9ca845462d9b
cshipley98 <claireshipley98@gmail.com>
2024-05-21 08:13:35 -0600

    handlebars

--- views/server.js
+++ views/server.js
@@ -3,3 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+    
