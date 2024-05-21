46d22745af6083987c13abea730732274d1d10de
cshipley98 <claireshipley98@gmail.com>
2024-05-21 08:18:51 -0600

    express

--- views/server.js
+++ views/server.js
@@ -28,3 +28,10 @@ const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(routes);
