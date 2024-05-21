ffefaf5dbe6912bea0af30a78e4764675963fab7
cshipley98 <claireshipley98@gmail.com>
2024-05-21 08:30:30 -0600

    creating connection

--- views/connection.js
+++ views/connection.js
@@ -3,3 +3,6 @@ const Sequelize = require('sequelize');
 
 // import dotenv for password security 
 require('dotenv').config();
+
+// create connection to our database, pass in MySql info for username/password
+let sequelize;
